ec3c906813e4805c0cfa1686c2dd716741fbb344
Vadim Yevlanov <vad.evlanov@gmail.com>
2023-01-05 14:02:36 +0100

    finished second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import storage from './localStorage';
 
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time' || 0;
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlay = ({ seconds }) => {
+  if (!seconds) return;
+
+  storage.save(VIDEO_CURRENT_TIME, seconds);
+};
+
+player.setCurrentTime(storage.get(VIDEO_CURRENT_TIME) || 0);
+
+player.on('timeupdate', throttle(onPlay, 1000));
